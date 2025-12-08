fc32485a177cecfb6e5f647159c60fade212c1aa
Filip Dabrowski <ezarus1@gmail.com>
2025-12-08 15:52:45 +0100

    Blueprint for the needed functions

---
+++ src/tryg_approx.cpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <vector>
+
+struct Point {
+    double x, y;
+};
+
+//==TO DO==
+
+//Function to calculate fourier value (approximation for the given x)
+
+double fourier_value() {
+    return 0;
+};
+
+//Function to calculate an (cos vector) and bn (sin vector)
+
+void calculate_fourier_()
+{
+
+}
+
+
+//Function to calculate Mean Squared Error between orignal points and approximation
+
+double calculate_MSE()
+{
+    return 0;
+}
+
+//Function to call approximation and MSE (on key press)
